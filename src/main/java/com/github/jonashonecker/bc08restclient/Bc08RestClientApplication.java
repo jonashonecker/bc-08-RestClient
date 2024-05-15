722d09109ef2b5bedb782bdbdcf6a33397cb7a1a
Jonas Honecker <honeckerjonas@gmail.com>
2024-05-15 10:41:01 +0200

    Initial commit

---
+++ src/main/java/com/github/jonashonecker/bc08restclient/Bc08RestClientApplication.java
@@ -0,0 +1,13 @@
+package com.github.jonashonecker.bc08restclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bc08RestClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bc08RestClientApplication.class, args);
+    }
+
+}
